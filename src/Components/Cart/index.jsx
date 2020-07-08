21a3d84a7c02e83e09a1d290610bdb31182ab95d
Deyvison J Paula <deyvisonjp@id.uff.br>
2020-07-08 19:06:35 -0300

    Modificacoes para build

--- src/Components/Cart/index.jsx
+++ src/Components/Cart/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import './style.css'
-import imgCompra from "../../assets/carrinho.png";
 
 const Cart = ( pokemon ) => {
    return (
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Header from "../../Components/Header";
 import Main from "../../Components/Main";
