b9d26f5bf5a123064cb43aad3d3c9973b6433aa7
Deyvison J Paula <deyvisonjp@id.uff.br>
2020-07-10 19:19:32 -0300

    Adicionando modal

--- src/Components/Cart/index.jsx
+++ src/Components/Cart/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Modal from 'react-modal'
 
 import './style.css'
 
@@ -19,6 +20,35 @@ const Cart = ({ resetarCarrinho, pokemons }) => {
       setTotalCompras(total)
    }, [pokemonsList])
 
+   Modal.setAppElement('#root')
+
+
+   function openModal() {
+      setIsOpen(true);
+   }
+
+   function afterOpenModal() {
+   }
+
+   function closeModal() {
+      resetarCarrinho()
+      setIsOpen(false);
+   }
+
+   const customStyles = {
+      content: {
+         top: '50%',
+         left: '50%',
+         right: 'auto',
+         bottom: 'auto',
+         marginRight: '-50%',
+         transform: 'translate(-50%, -50%)',
+         textAlign: 'center',
+      }, overlay: {
+         backgroundColor: 'rgb(18, 81, 131)'
+      },
+   };
+
    return (
       <div className="carrinho">
          <h1>Carrinho PokéGama</h1>
@@ -35,24 +65,39 @@ const Cart = ({ resetarCarrinho, pokemons }) => {
                   </div>
                   <div>  <h3>{pokemon.name}</h3> </div>
                   <div className="">
-                     <p>{pokemon.price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+                     <p>{pokemon.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</p>
                   </div>
                </div>
             ))
          }
+
+         <Modal
+            style={customStyles}
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            contentLabel="Modal para conclusão da compra"
+            onAfterOpen={afterOpenModal}
+         >
+            <div className="divModal">
+               <h1>PokéGama</h1>
+               <h2>Agradece sua compra</h2>
+               <h2>{totalCompras.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h2>
+               <button className="cardButton" onClick={closeModal}>Concluir</button>
+            </div>
+         </Modal>
 
          <div className="">
             <div className="total">
                <p>Total</p>
             </div>
             <div className="">
-               <p>{totalCompras.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+               <p>{totalCompras.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</p>
             </div>
          </div>
          <div className="">
             <div className="">
-               <button
-                  // onClick={openModal}
+               <button className="cardButton"
+                  onClick={openModal}
                   type='submit'
                >FINALIZAR</button>
             </div>
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -15,8 +15,8 @@ export default function Header() {
             <button className="btnBusca" > <img src={pokebola} alt="pokebola buscar"/> </button>
          </div>
          <nav className="nav_links">
-            <a href="https://github.com/deyvisonjp" className="cta"> <img width="45px" src={github}/></a>
-            <a href="https://www.linkedin.com/in/deyvisonjpaula/" className="cta"> <img width="45px" src={linkedin}/></a>
+            <a target='_blank' href="https://github.com/deyvisonjp" className="cta"> <img width="45px" src={github}/></a>
+            <a target='_blank' href="https://www.linkedin.com/in/deyvisonjpaula/" className="cta"> <img width="45px" src={linkedin}/></a>
          </nav>
       </header>
    )
