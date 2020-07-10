d30adaa973275cbca2462bcce09b19f78bcced6a
ContabilDP <ContabilDP>
2020-07-10 10:02:53 -0300

    Correção de responsividade

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './style.css'
+
+const Footer = () => {
+    return(
+        <div className='footer'>
+            <h4> © 2020 - Deyvison Jose de Paula </h4>
+            <p>Gama Academy - Projeto Loja Pokémon</p>
+        </div>
+    )
+}
+
+export default Footer;
